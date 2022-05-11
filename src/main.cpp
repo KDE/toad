5958b1d1a6c8f12a9cc9087abd5e270477333c60
Felipe Kinoshita <kinofhek@gmail.com>
2022-05-11 19:43:25 -0300

    Update bug address URL

--- src/main.cpp
+++ src/main.cpp
@@ -41,7 +41,7 @@ Q_DECL_EXPORT int main(int argc, char *argv[])
                          // Copyright Statement.
                          i18n("© 2022"));
     aboutData.addAuthor(i18nc("@info:credit", "Felipe Kinoshita"), i18nc("@info:credit", "Author"), QStringLiteral("kinofhek@gmail.com"), QStringLiteral("https://fhek.gitlab.io"));
-    aboutData.setBugAddress("https://invent.kde.org/fhek/tasks/-/issues/new");
+    aboutData.setBugAddress("https://invent.kde.org/utilities/toad/-/issues/new");
     KAboutData::setApplicationData(aboutData);
     QGuiApplication::setWindowIcon(QIcon::fromTheme(QStringLiteral("org.kde.tasks")));
 
