d9a747ee6372648eb316fe9e24cf6eeac155b619
Felipe Kinoshita <kinofhek@gmail.com>
2022-06-08 08:23:57 +0000

    Use correct project name in the Messages.sh file

--- Messages.sh
+++ Messages.sh
@@ -2,4 +2,4 @@
 # SPDX-FileCopyrightText: None
 # SPDX-License-Identifier: CC0-1.0
 
-$XGETTEXT `find . \( -name \*.cpp -o -name \*.h -o -name \*.qml \)` -o $podir/tasks.pot
+$XGETTEXT `find . \( -name \*.cpp -o -name \*.h -o -name \*.qml \)` -o $podir/toad.pot
