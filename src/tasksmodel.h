95c7766225fee0013f9338a7392043d7ef97fbe7
Nate Graham <nate@kde.org>
2022-10-17 10:16:22 -0600

    Revert "Remove completed tasks tooltip"
    This reverts commit fef94f0da90911f2dd6d434623739cc7a7f23017.
    
    This broke the ability to save tasks, which is fairly major. Since no
    fix has been forthcoming, let's revert for now.
    
    Fixes #19

--- src/tasksmodel.h
+++ src/tasksmodel.h
@@ -11,6 +11,7 @@
 class TasksModel : public QAbstractListModel
 {
     Q_OBJECT
+    Q_PROPERTY(int completedTasks READ completedTasks NOTIFY completedTasksChanged)
 
 public:
     enum Roles {
@@ -29,10 +30,17 @@ public:
     Q_INVOKABLE void remove(const int &index);
     Q_INVOKABLE void clear();
 
+    [[nodiscard]] int completedTasks() const
+    {
+        return m_completedTasks;
+    }
+    Q_SIGNAL void completedTasksChanged();
+
 protected:
     bool saveTasks() const;
     bool loadTasks();
 
 private:
     QList<Task> m_tasks;
+    int m_completedTasks = 0;
 };
